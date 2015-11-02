f46424c3d09f0a8cb42546ebeeee0059dda0caa3
fuwanli <ios-26@ACC8082A.ipt.aol.com>
2015-11-02 18:59:30 +0800

    测试2
    测试

--- FirstDemo/FirstDemo/ViewController.m
+++ FirstDemo/FirstDemo/ViewController.m
@@ -17,7 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"1111");
+
+        NSLog(@"1111");
+        NSLog(@"1111");
+        NSLog(@"1111");
+        NSLog(@"1111");
+        NSLog(@"1111");
+    
 }
 
 - (void)didReceiveMemoryWarning {
